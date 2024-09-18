207c04a11f49f34f39f3c3987be83fb65a47fed6
Gerardo de Leon <gerardo-leon10@hotmail.com>
2024-09-18 14:32:39 -0600

    Create custom hook randomColorGenerator and use it in charts

--- src/data/allExpensesData.ts
+++ src/data/allExpensesData.ts
@@ -1,3 +1,5 @@
+import useRandomColorGenerator from "../hooks/useRandomColorGenerator";
+
 export const initialExpenses = [
   {
     id: 1,
@@ -22,17 +24,16 @@ export const initialExpenses = [
   },
 ];
 
+const bgColors = initialExpenses.map(() => useRandomColorGenerator());
+console.log(bgColors);
+
 export const allChartData = {
   labels: ["Red", "Blue", "Yellow"],
   datasets: [
     {
       label: "My First Dataset",
       data: [300, 50, 100, 200],
-      backgroundColor: [
-        "rgb(255, 99, 132)",
-        "rgb(54, 162, 235)",
-        "rgb(255, 205, 86)",
-      ],
+      backgroundColor: bgColors,
       hoverOffset: 4,
     },
   ],
---
+++ src/hooks/useRandomColorGenerator.ts
@@ -0,0 +1,7 @@
+const useRandomColorGenerator = () => {
+  return `#${Math.floor(Math.random() * 0x1000000)
+    .toString(16)
+    .padStart(6, "0")}`;
+};
+
+export default useRandomColorGenerator;
